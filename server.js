e73d0c30538038640ef2f09ef8329f0fe979c0a8
Du <Sampan0103314@gmail.com>
2019-01-19 15:28:14 -0500

    modify signin endpoint

--- server.js
+++ server.js
@@ -46,11 +46,21 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {
-  if(req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-    res.status(200).json(database.users[0])
-  } else {
-    res.status(400).json('fail to login')
-  }
+  db.select('email', 'hash')
+    .from('login')
+    .where('email', '=', req.body.email)
+    .then(data => {
+      const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
+      if(isValid) {
+        return db.select('*')
+                  .from('users')
+                  .where('email', '=', req.body.email)
+                  .then(user => res.status(200).json(user[0]))
+      } else {
+        res.status(400).json('unable to login')
+      }
+    })
+    .catch(err => res.status(400).json('unable to login'))
 })
 
 app.post('/register', (req, res) => {
