50a9be1457b3673503149ec9be38b762d7a288f2
Du <Sampan0103314@gmail.com>
2019-01-19 13:24:46 -0500

    modify register endpoint and save password into login table

--- server.js
+++ server.js
@@ -54,16 +54,27 @@ app.post('/signin', (req, res) => {
 })
 
 app.post('/register', (req, res) => {
-  const { name, email } = req.body
-  db('users')
-    .returning('*')
-    .insert({
-      name: name,
-      email: email,
-      joined: new Date()
-    })
-    .then(user => res.json(user[0]))
-    .catch(err => res.status(400).json('unable to register'))
+  const { name, email, password } = req.body
+  const hash = bcrypt.hashSync(password)   //hashSync not hash
+
+  db.transaction(trx => {
+    trx.insert({email, hash})
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')     //return
+                .returning('*')
+                .insert({
+                  name: name,
+                  email: loginEmail[0],
+                  joined: new Date()
+                })
+                .then(user => res.json(user[0]))
+        })
+      .then(trx.commit)
+      .catch(trx.rollback)
+  })
+  .catch(err => res.status(400).json('unable to register'))
 })
 
 app.get('/profile/:id', (req, res) => {
