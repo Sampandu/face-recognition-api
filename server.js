95be1f0b8493e3ecb9fec4ecbd9b5abdc6bb512f
Du <Sampan0103314@gmail.com>
2019-01-29 09:54:30 +0800

    refactor image endpoint

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const knex = require('knex')
 const { handleRegister } = require('./handlers/register')
 const { handleSignin } = require('./handlers/signin')
 const { handleProfileGet } = require('./handlers/profile')
+const { handleImage } = require('./handlers/image')
 
 const db = knex({
   client: 'pg',
@@ -27,15 +28,7 @@ app.use(cors())
 app.post('/signin', (req, res) => handleSignin(req, res, db, bcrypt))
 app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt))
 app.get('/profile/:id', (req, res) => handleProfileGet(req, res, db))
-
-app.put('/image', (req, res) => {
-  const { id } = req.body
-  db('users').where('id', '=', id)
-    .increment('submition', 1)
-    .returning('submition')
-    .then(submition => res.json(submition))
-    .catch(err => res.status(400).json('error getting submition'))
-})
+app.put('/image', (req, res) => handleImage(req, res, db))
 
 app.listen(3001, () => {
   console.log('The server is listening on port 3001')
