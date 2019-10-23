dbdc5bec8d23ff887761e270d8868e3b1df7f6ad
Du <Sampan0103314@gmail.com>
2019-10-23 12:57:13 -0400

    update the port

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const bcrypt = require('bcrypt-nodejs')
-const cors = require('cors')
-const knex = require('knex')
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
 
-const { handleRegister } = require('./handlers/register')
-const { handleSignin } = require('./handlers/signin')
-const { handleProfileGet } = require('./handlers/profile')
-const { handleImage, handleAPIcall } = require('./handlers/image')
+const { handleRegister } = require('./handlers/register');
+const { handleSignin } = require('./handlers/signin');
+const { handleProfileGet } = require('./handlers/profile');
+const { handleImage, handleAPIcall } = require('./handlers/image');
 
 const db = knex({
   client: 'pg',
   connection: {
     host: '127.0.0.1',
-    user:'du',
-    password:'',
-    database:'face-recognition'
-  }
-})
+    user: 'du',
+    password: '',
+    database: 'face-recognition',
+  },
+});
 
-const app = express()
+const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.post('/signin', (req, res) => handleSignin(req, res, db, bcrypt))
-app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt))
-app.get('/profile/:id', (req, res) => handleProfileGet(req, res, db))
-app.put('/image', (req, res) => handleImage(req, res, db))
-app.post('/imageurl', (req, res) => handleAPIcall(req, res))
+app.post('/signin', (req, res) => handleSignin(req, res, db, bcrypt));
+app.post('/register', (req, res) => handleRegister(req, res, db, bcrypt));
+app.get('/profile/:id', (req, res) => handleProfileGet(req, res, db));
+app.put('/image', (req, res) => handleImage(req, res, db));
+app.post('/imageurl', (req, res) => handleAPIcall(req, res));
 
-app.listen(3001, () => {
-  console.log('The server is listening on port 3001')
-})
+app.listen(process.env.PORT || 3001, () => {
+  console.log(`The server is listening on port ${process.env.PORT} `);
+});
